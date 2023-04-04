2fab462988c1050dab0c666377e93687b9f3391c
Jaakko Syrjämäki <jaakkosyrjamaki92@hotmail.com>
2023-04-04 16:37:43 +0300

    task ready

--- part_04blog/controllers/blogs.js
+++ part_04blog/controllers/blogs.js
@@ -1,20 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/api/blogs', (request, response) => {
+blogsRouter.get('/', (request, response) => {
     Blog.find({}).then(blogs => {
         response.json(blogs)
     })
 })
   
-blogsRouter.post('/api/blogs', (request, response) => {
+blogsRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
+    blog.save().then(result => {
         response.status(201).json(result)
-        })
+    })
 })
 
 module.exports = blogsRouter
